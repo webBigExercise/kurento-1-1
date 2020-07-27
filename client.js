a72a2688bcff02093b80b5409a620b399f88b501
quangbm <quangbm@gviet.vn>
2020-07-27 11:53:34 +0700

    done

---
+++ client.js
@@ -0,0 +1,82 @@
+const io = require('socket.io-client')
+const kurentoUtils = require('kurento-utils')
+
+const wsUrl = `https://localhost:3000`
+const socket = io(wsUrl)
+let webRtcPeer
+
+socket.on('connect', () => {
+  console.log('connected')
+  document.querySelector('#username').innerText = `Your id is ${socket.id}`
+
+})
+
+document.querySelector('#call-btn').onclick = function makeCall() {
+  const webRtcPeerOptions = {
+    localVideo: document.querySelector('#videoInput'),
+    remoteVideo: document.querySelector('#videoOutput'),
+
+    //browser collect ice candidate (network connection)
+    onicecandidate: (candidate) =>
+      socket.emit('client-send-ice-candidate', {
+        data: { candidate },
+      }),
+  }
+
+  webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(
+    webRtcPeerOptions,
+    function (err) {
+      if (err) return console.error(err)
+      this.generateOffer((err, sdp) => {
+        if (err) return console.error(err)
+
+        socket.emit('client-make-call', {
+          data: {
+            sdp,
+            callerId: socket.id,
+            calleeId: document.querySelector('#to').value,
+          },
+        })
+      })
+    }
+  )
+}
+
+socket.on('client-have-incoming-call', async ({data}) => {
+  const webRtcPeerOptions = {
+    localVideo: document.querySelector('#videoInput'),
+    remoteVideo: document.querySelector('#videoOutput'),
+
+    //browser collect ice candidate (network connection)
+    onicecandidate: (candidate) =>
+      socket.emit('client-send-ice-candidate', {
+        data: { candidate },
+      }),
+  }
+
+  webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(
+    webRtcPeerOptions,
+    function (err) {
+      if (err) return console.error(err)
+      this.generateOffer((err, sdp) => {
+        if (err) return console.error(err)
+
+        socket.emit('client-accept-call', {
+          data: {
+            sdp,
+            callerId: data.callerId,
+            calleeId: data.calleeId,
+          },
+        })
+      })
+    }
+  )
+})
+
+socket.on('server-send-kurento-candidate', ({data}) => {
+  webRtcPeer.addIceCandidate(data.candidate)
+})
+
+socket.on('start-communication', ({data}) => {
+  webRtcPeer.processAnswer(data.sdp)
+})
